a39d0299519d9ca3e8b411686eb468f9676f0bcf
ilya_ermakov <37378410+Std1o@users.noreply.github.com>
2024-05-25 22:19:59 +0300

    Update MainScreen.kt

--- app/src/main/java/com/example/cinema/presentation/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/cinema/presentation/ui/screens/MainScreen.kt
@@ -1,13 +1,14 @@
 package com.example.cinema.presentation.ui.screens
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -29,41 +30,51 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
-@OptIn(ExperimentalFoundationApi::class)
-@Destination<RootGraph>(start = true) // sets this as a destination of the "root" nav graph
+
+@Destination<RootGraph>(start = true)
 @Composable
 fun MainScreen(navigator: DestinationsNavigator, modifier: Modifier) {
     val viewModel = hiltViewModel<MoviesViewModel>()
     val movies by viewModel.uiState.collectAsState()
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         iTems(movies, key = { it }) { movie ->
-            Box(
+            val shape = RoundedCornerShape(5.dp)
+            Card(
+                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                shape = shape,
                 modifier = Modifier
-                    .animateItemPlacement()
                     .padding(vertical = 10.dp, horizontal = 16.dp)
-                    .height(150.dp)
                     .fillMaxWidth()
-                    .clip(RoundedCornerShape(10.dp))
             ) {
-                AsyncImage(
-                    model = movie.poster.url,
-                    contentDescription = "Translated description of what the image contains",
-                    modifier = Modifier.fillMaxWidth(),
-                    contentScale = ContentScale.FillWidth
-                )
-                Text(
-                    text = movie.name,
-                    modifier = Modifier.padding(16.dp),
-                    fontSize = 20.sp,
-                    color = Color.White,
-                    style = TextStyle(
-                        shadow = Shadow(
-                            Color.Black,
-                            Offset(3.0f, 4.95f),
-                            1.0f
+                Column(
+                    modifier = Modifier
+                        .padding(vertical = 10.dp, horizontal = 16.dp)
+                        .height(200.dp)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp))
+                ) {
+                    AsyncImage(
+                        model = movie.poster.url,
+                        contentDescription = "Translated description of what the image contains",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(150.dp),
+                        contentScale = ContentScale.FillWidth
+                    )
+                    Text(
+                        text = movie.name,
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        style = TextStyle(
+                            shadow = Shadow(
+                                Color.Black,
+                                Offset(3.0f, 4.95f),
+                                1.0f
+                            )
                         )
                     )
-                )
+                }
             }
         }
     }
