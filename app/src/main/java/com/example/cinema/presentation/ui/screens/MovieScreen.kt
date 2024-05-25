a89c346856a669a7f99e23e991f7ecde8cf4f66f
ilya_ermakov <37378410+Std1o@users.noreply.github.com>
2024-05-26 01:26:59 +0300

    Update MovieScreen.kt

--- app/src/main/java/com/example/cinema/presentation/ui/screens/MovieScreen.kt
+++ app/src/main/java/com/example/cinema/presentation/ui/screens/MovieScreen.kt
@@ -1,6 +1,9 @@
 package com.example.cinema.presentation.ui.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -8,6 +11,7 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -37,35 +41,43 @@ fun MovieScreen() {
             prepare()
         }
 
-        DisposableEffect(key1 = AndroidView(modifier = Modifier.fillMaxSize(), factory = {
-            StyledPlayerView(context).apply {
-                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
-                player = exoPlayer
-            }
-        }), effect = {
-            val observer = LifecycleEventObserver { _, event ->
-                when (event) {
-                    Lifecycle.Event.ON_RESUME -> {
-                        Log.e("LIFECYCLE", "resumed")
-                        exoPlayer.play()
-                    }
-
-                    Lifecycle.Event.ON_PAUSE -> {
-                        Log.e("LIFECYCLE", "paused")
-                        exoPlayer.stop()
-                    }
-
-                    else -> {}
+        Box(modifier = Modifier
+                .padding(top = 50.dp)
+                .height(300.dp)
+        ) {
+            AndroidView(factory = {
+                StyledPlayerView(context).apply {
+                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+                    player = exoPlayer
                 }
-            }
+            })
+        }
 
-            val lifecycle = lifecycleOwner.value.lifecycle
-            lifecycle.addObserver(observer)
+        DisposableEffect(
+            key1 = Unit, effect = {
+                val observer = LifecycleEventObserver { _, event ->
+                    when (event) {
+                        Lifecycle.Event.ON_RESUME -> {
+                            Log.e("LIFECYCLE", "resumed")
+                            exoPlayer.play()
+                        }
 
-            onDispose {
-                exoPlayer.release()
-                lifecycle.removeObserver(observer)
-            }
-        })
+                        Lifecycle.Event.ON_PAUSE -> {
+                            Log.e("LIFECYCLE", "paused")
+                            exoPlayer.stop()
+                        }
+
+                        else -> {}
+                    }
+                }
+
+                val lifecycle = lifecycleOwner.value.lifecycle
+                lifecycle.addObserver(observer)
+
+                onDispose {
+                    exoPlayer.release()
+                    lifecycle.removeObserver(observer)
+                }
+            })
     }
 }
