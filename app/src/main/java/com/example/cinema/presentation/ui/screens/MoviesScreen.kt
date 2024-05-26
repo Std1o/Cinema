3f751e01914bf4b172ce386a50e77837d69a8a4f
ilya_ermakov <37378410+Std1o@users.noreply.github.com>
2024-05-27 00:27:31 +0300

    SearchAppBar

--- app/src/main/java/com/example/cinema/presentation/ui/screens/MoviesScreen.kt
+++ app/src/main/java/com/example/cinema/presentation/ui/screens/MoviesScreen.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,13 +23,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.example.cinema.R
 import com.example.cinema.common.iTems
+import com.example.cinema.presentation.ui.components.SearchAppBar
 import com.example.cinema.presentation.viewmodel.MoviesViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
@@ -39,64 +46,73 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 fun MoviesScreen(navigator: DestinationsNavigator) {
     val viewModel = hiltViewModel<MoviesViewModel>()
     val movies by viewModel.uiState.collectAsState()
-    LazyColumn(modifier = Modifier.fillMaxSize().padding(top = 30.dp)) {
-        iTems(movies, key = { it }) { movie ->
-            val shape = RoundedCornerShape(5.dp)
-            Card(
-                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
-                shape = shape,
-                modifier = Modifier
-                    .padding(vertical = 10.dp, horizontal = 16.dp)
-                    .fillMaxWidth()
-                    .clickable {
-                        navigator.navigate(MovieScreenDestination(movie))
-                    }
-            ) {
-                Column(
+    Scaffold(
+        topBar = {
+            SearchAppBar {
+//                viewModel.searchPrefix = it
+//                viewModel.getResults()
+            }
+        }
+    ) { contentPadding ->
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(contentPadding).padding(top = 30.dp)) {
+            iTems(movies, key = { it }) { movie ->
+                val shape = RoundedCornerShape(5.dp)
+                Card(
+                    elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                    shape = shape,
                     modifier = Modifier
                         .padding(vertical = 10.dp, horizontal = 16.dp)
-                        .height(300.dp)
                         .fillMaxWidth()
-                        .clip(RoundedCornerShape(10.dp))
+                        .clickable {
+                            navigator.navigate(MovieScreenDestination(movie))
+                        }
                 ) {
-                    AsyncImage(
-                        model = movie.poster.url,
-                        contentDescription = "Translated description of what the image contains",
+                    Column(
                         modifier = Modifier
+                            .padding(vertical = 10.dp, horizontal = 16.dp)
+                            .height(300.dp)
                             .fillMaxWidth()
-                            .height(150.dp)
-                            .clip(RoundedCornerShape(10.dp)),
-                        contentScale = ContentScale.FillWidth
-                    )
-                    Row {
-                        Text(
-                            text = movie.name,
-                            modifier = Modifier.padding(top = 8.dp),
-                            fontSize = 20.sp,
-                            color = Color.White,
-                            style = TextStyle(
-                                shadow = Shadow(
-                                    Color.Black,
-                                    Offset(3.0f, 4.95f),
-                                    1.0f
+                            .clip(RoundedCornerShape(10.dp))
+                    ) {
+                        AsyncImage(
+                            model = movie.poster.url,
+                            contentDescription = "Translated description of what the image contains",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(150.dp)
+                                .clip(RoundedCornerShape(10.dp)),
+                            contentScale = ContentScale.FillWidth
+                        )
+                        Row {
+                            Text(
+                                text = movie.name,
+                                modifier = Modifier.padding(top = 8.dp),
+                                fontSize = 20.sp,
+                                color = Color.White,
+                                style = TextStyle(
+                                    shadow = Shadow(
+                                        Color.Black,
+                                        Offset(3.0f, 4.95f),
+                                        1.0f
+                                    )
                                 )
                             )
-                        )
+                            Text(
+                                text = "• ${movie.year}",
+                                modifier = Modifier.padding(top = 10.dp, start = 8.dp),
+                                color = Color.Gray,
+                            )
+                        }
                         Text(
-                            text = "• ${movie.year}",
-                            modifier = Modifier.padding(top = 10.dp, start = 8.dp),
+                            text = movie.genres.joinToString { it.name },
                             color = Color.Gray,
                         )
+                        Text(
+                            text = movie.shortDescription ?: movie.description,
+                            color = Color.White,
+                            fontSize = 12.sp
+                        )
                     }
-                    Text(
-                        text = movie.genres.joinToString { it.name },
-                        color = Color.Gray,
-                    )
-                    Text(
-                        text = movie.shortDescription ?: movie.description,
-                        color = Color.White,
-                        fontSize = 12.sp
-                    )
                 }
             }
         }
