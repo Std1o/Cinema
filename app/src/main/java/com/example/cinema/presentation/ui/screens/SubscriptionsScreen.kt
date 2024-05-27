f07c6e94042e03beae55fe415dda388086285031
ilya_ermakov <37378410+Std1o@users.noreply.github.com>
2024-05-27 22:48:58 +0300

    SubscriptionsScreen loader

--- app/src/main/java/com/example/cinema/presentation/ui/screens/SubscriptionsScreen.kt
+++ app/src/main/java/com/example/cinema/presentation/ui/screens/SubscriptionsScreen.kt
@@ -1,47 +1,21 @@
 package com.example.cinema.presentation.ui.screens
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImage
-import com.example.cinema.common.iTems
+import com.example.cinema.presentation.ui.components.LoadingIndicator
 import com.example.cinema.presentation.ui.components.MoviesList
-import com.example.cinema.presentation.ui.components.SearchAppBar
-import com.example.cinema.presentation.viewmodel.MoviesViewModel
 import com.example.cinema.presentation.viewmodel.SubscriptionsViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
-import com.ramcosta.composedestinations.generated.destinations.MovieScreenDestination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 
 @Destination<RootGraph>
 @Composable
 fun SubscriptionsScreen() {
     val viewModel = hiltViewModel<SubscriptionsViewModel>()
-    val movies by viewModel.uiState.collectAsState()
-    MoviesList(movies = movies) {}
+    val uiState by viewModel.uiState.collectAsState()
+    MoviesList(movies = uiState.movies) {}
+    if (uiState.isLoading) LoadingIndicator()
 }
--- app/src/main/java/com/example/cinema/presentation/viewmodel/SubscriptionsViewModel.kt
+++ app/src/main/java/com/example/cinema/presentation/viewmodel/SubscriptionsViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cinema.domain.models.Movie
 import com.example.cinema.domain.repository.MoviesRepository
+import com.example.cinema.presentation.ui.models.MoviesUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,13 +22,13 @@ class SubscriptionsViewModel @Inject constructor(
         getMovies()
     }
 
-    private val _uiState = MutableStateFlow<List<Movie>>(emptyList())
+    private val _uiState = MutableStateFlow(MoviesUIState())
     val uiState = _uiState.asStateFlow()
 
     private fun getMovies() {
         viewModelScope.launch {
             val movies = repository.getMovies(1, "фантастика").movies
-            _uiState.value = movies
+            _uiState.update { it.copy(movies = movies, isLoading = false) }
         }
     }
 }
