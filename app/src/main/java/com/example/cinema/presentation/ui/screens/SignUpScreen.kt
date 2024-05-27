61620c215b2df0abc78a617ac96809c6a85e57fe
ilya_ermakov <37378410+Std1o@users.noreply.github.com>
2024-05-27 21:19:17 +0300

    Update SignUpScreen.kt

--- app/src/main/java/com/example/cinema/presentation/ui/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/cinema/presentation/ui/screens/SignUpScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -19,17 +18,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cinema.presentation.ui.navigation.AuthGraph
-import com.example.cinema.presentation.ui.theme.Purple80
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination<AuthGraph>()
@@ -41,7 +37,9 @@ fun SignUpScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        val fio = remember { mutableStateOf(TextFieldValue()) }
         val email = remember { mutableStateOf(TextFieldValue()) }
+        val phone = remember { mutableStateOf(TextFieldValue()) }
         val password = remember { mutableStateOf(TextFieldValue()) }
         val confirmPassword = remember { mutableStateOf(TextFieldValue()) }
 
@@ -49,9 +47,24 @@ fun SignUpScreen() {
 
         Spacer(modifier = Modifier.height(20.dp))
         TextField(
+            label = { Text(text = "ФИО") },
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words),
+            value = fio.value,
+            onValueChange = { fio.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
             label = { Text(text = "E-mail") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
             value = email.value,
             onValueChange = { email.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Телефон") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            value = phone.value,
+            onValueChange = { phone.value = it })
 
         Spacer(modifier = Modifier.height(20.dp))
         TextField(
@@ -60,8 +73,8 @@ fun SignUpScreen() {
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             onValueChange = { password.value = it })
-        Spacer(modifier = Modifier.height(20.dp))
 
+        Spacer(modifier = Modifier.height(20.dp))
         TextField(
             label = { Text(text = "Повторите пароль") },
             value = confirmPassword.value,
