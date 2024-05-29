9bec45c8ac7adc7c9db1dfe64404dd551aaf6ea2
ilya_ermakov <37378410+Std1o@users.noreply.github.com>
2024-05-30 00:55:34 +0300

    upd FilmAddingScreen

--- app/src/main/java/com/example/cinema/presentation/ui/screens/film_adding/FilmAddingScreen.kt
+++ app/src/main/java/com/example/cinema/presentation/ui/screens/film_adding/FilmAddingScreen.kt
@@ -1,9 +1,11 @@
 package com.example.cinema.presentation.ui.screens.film_adding
 
+import android.net.Uri
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -30,6 +34,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.TextFieldValue
@@ -38,6 +43,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import coil.compose.rememberAsyncImagePainter
 import com.example.cinema.presentation.ui.components.ExoPlayerLifecycleObserver
 import com.example.cinema.presentation.ui.theme.Purple40
 import com.example.cinema.presentation.viewmodel.FilmAddingViewModel
@@ -63,6 +69,11 @@ fun FilmAddingScreen(navigator: DestinationsNavigator) {
     }
     val viewModel = hiltViewModel<FilmAddingViewModel>()
     val genres by viewModel.genres.collectAsState()
+    var imageData by remember { mutableStateOf(Uri.EMPTY) }
+    val imagePicker =
+        rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
+            imageData = it
+        }
 
     Scaffold(
         snackbarHost = {
@@ -86,7 +97,10 @@ fun FilmAddingScreen(navigator: DestinationsNavigator) {
                 .padding(contentPadding)
         ) {
             Column(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 55.dp)
+                    .verticalScroll(rememberScrollState()),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 if (!videoIsPicked) {
@@ -129,10 +143,22 @@ fun FilmAddingScreen(navigator: DestinationsNavigator) {
                 Text("Жанр", fontSize = 12.sp)
                 GenresDropDown(genres)
 
-                Button(
-                    modifier = Modifier.padding(contentPadding),
-                    onClick = { videoPicker.launch("image/*") }) {
-                    Text("Добавить превью")
+                if (imageData == Uri.EMPTY) {
+                    Button(
+                        modifier = Modifier.padding(contentPadding),
+                        onClick = { imagePicker.launch("image/*") }) {
+                        Text("Добавить превью")
+                    }
+                } else {
+                    Image(
+                        painter = rememberAsyncImagePainter(imageData),
+                        contentScale = ContentScale.FillWidth,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(vertical = 16.dp)
+                            .height(200.dp)
+                            .width(300.dp)
+                    )
                 }
             }
 
@@ -140,7 +166,7 @@ fun FilmAddingScreen(navigator: DestinationsNavigator) {
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .width(200.dp)
-                    .padding(bottom = 16.dp),
+                    .padding(bottom = 8.dp),
                 border = BorderStroke(1.dp, Purple40),
                 onClick = {
                     Toast.makeText(context, "Фильм успешно добавлен", Toast.LENGTH_SHORT).show()
