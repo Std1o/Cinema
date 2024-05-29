ab4551703f16e74de29c4575aaf2528075243d56
ilya_ermakov <37378410+Std1o@users.noreply.github.com>
2024-05-30 00:35:29 +0300

    Update FilmAddingScreen.kt

--- app/src/main/java/com/example/cinema/presentation/ui/screens/film_adding/FilmAddingScreen.kt
+++ app/src/main/java/com/example/cinema/presentation/ui/screens/film_adding/FilmAddingScreen.kt
@@ -3,14 +3,17 @@ package com.example.cinema.presentation.ui.screens.film_adding
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -36,6 +39,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.example.cinema.presentation.ui.components.ExoPlayerLifecycleObserver
+import com.example.cinema.presentation.ui.theme.Purple40
 import com.example.cinema.presentation.viewmodel.FilmAddingViewModel
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
@@ -76,57 +80,73 @@ fun FilmAddingScreen(navigator: DestinationsNavigator) {
             }
         )
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding)
         ) {
-            if (!videoIsPicked) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (!videoIsPicked) {
+                    Button(
+                        modifier = Modifier.padding(contentPadding),
+                        onClick = { videoPicker.launch("video/*") }) {
+                        Text("Выбрать видео")
+                    }
+                } else {
+                    Box(
+                        modifier = Modifier
+                            .padding(top = 50.dp)
+                            .height(200.dp)
+                    ) {
+                        AndroidView(factory = {
+                            StyledPlayerView(context).apply {
+                                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+                                player = exoPlayer
+                            }
+                        })
+                    }
+                }
+
+                val name = remember { mutableStateOf(TextFieldValue()) }
+                val description = remember { mutableStateOf(TextFieldValue()) }
+
+                Spacer(modifier = Modifier.height(20.dp))
+                TextField(
+                    label = { Text(text = "Название фильма") },
+                    keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words),
+                    value = name.value,
+                    onValueChange = { name.value = it })
+
+                Spacer(modifier = Modifier.height(20.dp))
+                TextField(
+                    label = { Text(text = "Описание фильма") },
+                    keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+                    value = description.value,
+                    onValueChange = { description.value = it })
+                Text("Жанр", fontSize = 12.sp)
+                GenresDropDown(genres)
+
                 Button(
                     modifier = Modifier.padding(contentPadding),
-                    onClick = { videoPicker.launch("video/*") }) {
-                    Text("Выбрать видео")
-                }
-            } else {
-                Box(
-                    modifier = Modifier
-                        .padding(top = 50.dp)
-                        .height(200.dp)
-                ) {
-                    AndroidView(factory = {
-                        StyledPlayerView(context).apply {
-                            resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
-                            player = exoPlayer
-                        }
-                    })
+                    onClick = { videoPicker.launch("image/*") }) {
+                    Text("Добавить превью")
                 }
             }
 
-            val name = remember { mutableStateOf(TextFieldValue()) }
-            val description = remember { mutableStateOf(TextFieldValue()) }
-
-            Spacer(modifier = Modifier.height(20.dp))
-            TextField(
-                label = { Text(text = "Название фильма") },
-                keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words),
-                value = name.value,
-                onValueChange = { name.value = it })
-
-            Spacer(modifier = Modifier.height(20.dp))
-            TextField(
-                label = { Text(text = "Описание фильма") },
-                keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
-                value = description.value,
-                onValueChange = { description.value = it })
-            Text("Жанр", fontSize = 12.sp)
-            GenresDropDown(genres)
-
-            Button(
-                modifier = Modifier.padding(vertical = 16.dp),
+            OutlinedButton(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .width(200.dp)
+                    .padding(bottom = 16.dp),
+                border = BorderStroke(1.dp, Purple40),
                 onClick = {
                     Toast.makeText(context, "Фильм успешно добавлен", Toast.LENGTH_SHORT).show()
                     navigator.popBackStack()
                 }) {
-                Text("Добавить")
+                Text("Сохранить", color = Purple40)
             }
         }
     }
